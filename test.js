3e6abf24558dee44aad29fd4dc0fbd10a464ba21
unknown <interkp12@gmail.com>
2022-08-24 12:21:50 +0900

    test

---
+++ test.js
@@ -0,0 +1,37 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+
+const section1 = document.querySelector('.box1');
+const section2 = document.querySelector('.box2');
+const section3 = document.querySelector('.box3');
+const section4 = document.querySelector('.box4');
+
+btn1.addEventListener('click', () => {
+    scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+btn2.addEventListener('click', () => {
+    scrollTo({
+        top: 970,
+        behavior: "smooth"
+    });
+});
+
+btn3.addEventListener('click', () => {
+    scrollTo({
+        top: 1940,
+        behavior: "smooth"
+    });
+});
+
+btn4.addEventListener('click', () => {
+    scrollTo({
+        top: 3380,
+        behavior: "smooth"
+    });
+});
